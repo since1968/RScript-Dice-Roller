132272da2e28bd1e52785a384ac71aa8c9382e02
since1968 <marc.garrett@gmail.com>
2015-11-26 11:58:06 -0500

    First working copy
    Expects three parameters and has no error handling.

--- roll.R
+++ roll.R
@@ -1,8 +1,26 @@
 # roll.R
 # Rolls a specified number of dice and outputs to command line
 
+# usage:
+# To roll 3d6+5:
+# Rscript roll.R 3 6 5
+# To roll 3d6
+# Rscript roll.R 3 6 0
+
 # use this line to test arguments:
-# Rscript roll.R 10
-print(commandArgs(TRUE)[1])
+# Rscript roll.R 1d6+10
+# print(commandArgs(TRUE)[1])
 # output is:
-# [1] "10"
+# [1] "1d6+10"
+
+
+args <- commandArgs(trailingOnly = TRUE)
+# rnorm(n=as.numeric(args[1]), mean=as.numeric(args[2]))
+
+
+# die = as.numeric(args[2])
+
+monster <- sample(1:as.numeric(args[2]), as.numeric(args[1]), replace = T)
+monster
+monster <- sum(monster) + as.numeric(args[3])
+monster
