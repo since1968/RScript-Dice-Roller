5e41533b7faa39dfb5fab3044d1ef4a5a2b3fb4a
since1968 <marc.garrett@gmail.com>
2015-11-26 12:28:40 -0500

    Third parameter optonal

--- roll.R
+++ roll.R
@@ -2,25 +2,24 @@
 # Rolls a specified number of dice and outputs to command line
 
 # usage:
-# To roll 3d6+5:
-# Rscript roll.R 3 6 5
 # To roll 3d6
-# Rscript roll.R 3 6 0
-
-# use this line to test arguments:
-# Rscript roll.R 1d6+10
-# print(commandArgs(TRUE)[1])
-# output is:
-# [1] "1d6+10"
-
+# Rscript roll.R 3 6
+# To roll 3d6+5 include optional third argument:
+# Rscript roll.R 3 6 5
 
 args <- commandArgs(trailingOnly = TRUE)
-# rnorm(n=as.numeric(args[1]), mean=as.numeric(args[2]))
+# assume there's no modifier
+modifier <- 0
 
-
-# die = as.numeric(args[2])
-
+sprintf("You rolled:")
 monster <- sample(1:as.numeric(args[2]), as.numeric(args[1]), replace = T)
 monster
-monster <- sum(monster) + as.numeric(args[3])
-monster
+
+
+# check for optional third argument, otherwise just sum the rolls
+if(!is.na(args[3])) {
+	modifier <- as.numeric(args[3])
+	}
+
+monster <- sum(monster) + modifier
+sprintf("Your total: %s", monster)
