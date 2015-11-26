fe5077aeba6948594f4a7cae15a2bbb3e3efbe41
since1968 <marc.garrett@gmail.com>
2015-11-26 11:46:20 -0500

    Initial commit
    Simple test to output argument.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RScript-Dice-Roller
 Specify number, sides, and modifier from the command line.
+
+See Rscript reference: https://stat.ethz.ch/R-manual/R-devel/library/utils/html/Rscript.html
---
+++ roll.R
@@ -0,0 +1,8 @@
+# roll.R
+# Rolls a specified number of dice and outputs to command line
+
+# use this line to test arguments:
+# Rscript roll.R 10
+print(commandArgs(TRUE)[1])
+# output is:
+# [1] "10"
